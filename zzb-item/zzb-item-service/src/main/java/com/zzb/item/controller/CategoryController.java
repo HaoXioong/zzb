f47cea2ed916940e1d7a541d6e20e5b946432ce5
xionghao <2576181740@qq.com>
2021-04-16 15:42:58 +0800

    添加增删改查

--- zzb-item/zzb-item-service/src/main/java/com/zzb/item/controller/CategoryController.java
+++ zzb-item/zzb-item-service/src/main/java/com/zzb/item/controller/CategoryController.java
@@ -1,10 +1,10 @@
 package com.zzb.item.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.zzb.common.api.Result;
 import com.zzb.item.pojo.Category;
 import com.zzb.item.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("category")
 public class CategoryController {
 
@@ -44,9 +44,52 @@ public class CategoryController {
 //        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
+    /**
+     * 创建分类
+     * @param category
+     * @return
+     */
     @PostMapping("addCategory")
-    public ResponseEntity<Object> addCategory(@RequestBody Category category){
-
-        return ResponseEntity.ok().build();
+    public Result<Integer> addCategory(@RequestBody Category category) {
+        if(category.getParentId()==null||category.getName()==null){
+            return Result.validateFailed("参数错误！");
+        }
+        Integer num = this.categoryService.addCategory(category);
+        if(num > 0){
+            return Result.success(num);
+        }else{
+            return Result.failed();
+        }
     }
+
+    @PostMapping("editCategory")
+    public Result<Integer> editCategory(@RequestBody String str){
+        Long id = JSON.parseObject(str).getLong("id");
+        String name = JSON.parseObject(str).getString("name");
+        if(id==null||id<0||name==null){
+            return Result.validateFailed("参数错误！");
+        }
+        Integer num = this.categoryService.editCategory(id, name);
+        if(num > 0){
+            return Result.success(num);
+        }else{
+            return Result.failed();
+        }
+    }
+
+    @PostMapping("delCategory")
+    public Result<Integer> delCategory(@RequestBody String str){
+        Long id = JSON.parseObject(str).getLong("id");
+        if(id==null||id<0){
+            return Result.validateFailed("参数错误！");
+        }
+        Integer num = this.categoryService.delCategory(id);
+        if(num > 0){
+            return Result.success(num);
+        }else{
+            return Result.failed();
+        }
+    }
+
+
 }
--- zzb-item/zzb-item-service/src/main/java/com/zzb/item/service/CategoryService.java
+++ zzb-item/zzb-item-service/src/main/java/com/zzb/item/service/CategoryService.java
@@ -24,4 +24,33 @@ public class CategoryService {
         record.setParentId(pid);
         return this.categoryMapper.select(record);
     }
+
+    /**
+     * 根据父节点添加子节点
+     * @param category
+     * @return
+     */
+    public int addCategory(Category category){
+        return this.categoryMapper.insert(category);
+    }
+
+    /**
+     * 根据id修改名称
+     * @param id
+     * @param name
+     * @return
+     */
+    public int editCategory(Long id, String name){
+        Category category = new Category();
+        category.setId(id);
+        Category record = this.categoryMapper.selectOne(category);
+        record.setName(name);
+        return this.categoryMapper.updateByPrimaryKey(record);
+    }
+
+    public int delCategory(Long id) {
+        Category category = new Category();
+        category.setId(id);
+        return this.categoryMapper.delete(category);
+    }
 }
